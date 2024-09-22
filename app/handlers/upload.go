973486ad607c7e4d5bdedbeda26f3f7226102a79
Oscar Ek <oscar.ek.ek@partner.bmw.de>
2024-09-22 15:39:05 +0200

    vision with bytes

--- app/handlers/upload.go
+++ app/handlers/upload.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"smartquiz/app/ai"
+	"smartquiz/app/db"
+	"smartquiz/app/types"
 
 	"github.com/anthdm/superkit/kit"
 	_ "github.com/mattn/go-sqlite3" // Import SQLite driver
@@ -23,7 +24,7 @@ func HandleUpload(kit *kit.Kit) error {
 	fmt.Println("succesfully parsed file")
 
 	// FormFile returns the first file for the given key 'picture'
-	file, handler, err := kit.Request.FormFile("file")
+	file, _, err := kit.Request.FormFile("file")
 	if err != nil {
 		fmt.Println("error when fromfile", err)
 		http.Error(kit.Response, err.Error(), http.StatusInternalServerError)
@@ -32,27 +33,51 @@ func HandleUpload(kit *kit.Kit) error {
 	defer file.Close()
 
 	fmt.Println("succesfully fromfile")
-	// Create a new file in the current working directory
-	dst, err := os.Create(handler.Filename)
+
+	// Read file contents into a byte slice
+	fileBytes, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("error when creating file", err)
+		fmt.Println("error when reading file bytes", err)
 		http.Error(kit.Response, err.Error(), http.StatusInternalServerError)
 		return err
 	}
-	defer dst.Close()
-	fmt.Println("succesfully creating file")
 
-	// Copy the uploaded file to the destination file
-	if _, err := io.Copy(dst, file); err != nil {
-		fmt.Println("error when saving file", err)
-		http.Error(kit.Response, err.Error(), http.StatusInternalServerError)
+	// Now you have the file as bytes in fileBytes
+	fmt.Println("successfully read file into bytes")
+	visionRes := ai.ReadPicture(fileBytes)
+	germanWord := types.GermanWord{
+		DifficultyLevel: "",
+		GermanWord:      visionRes.Glossary,
+		Definition:      visionRes.Definition,
+	}
+	err = db.Get().Save(&germanWord).Error
+	if err != nil {
 		return err
 	}
-	fmt.Println("succesfully saving file")
-
-	// Optionally, respond back to the client
-	fmt.Fprintf(kit.Response, "File uploaded successfully: %+v", handler.Filename)
-
-	ai.StartVision(handler.Filename)
-	return nil
+	return kit.Redirect(http.StatusSeeOther, "/track")
 }
+
+//	// Create a new file in the current working directory
+//	dst, err := os.Create(handler.Filename)
+//	if err != nil {
+//		fmt.Println("error when creating file", err)
+//		http.Error(kit.Response, err.Error(), http.StatusInternalServerError)
+//		return err
+//	}
+//	defer dst.Close()
+//	fmt.Println("succesfully creating file")
+//
+//	// Copy the uploaded file to the destination file
+//	if _, err := io.Copy(dst, file); err != nil {
+//		fmt.Println("error when saving file", err)
+//		http.Error(kit.Response, err.Error(), http.StatusInternalServerError)
+//		return err
+//	}
+//	fmt.Println("succesfully saving file")
+//
+//	// Optionally, respond back to the client
+//	fmt.Fprintf(kit.Response, "File uploaded successfully: %+v", handler.Filename)
+//
+//	ai.StartVision(handler.Filename)
+//	return nil
+//}
